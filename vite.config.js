013294d832174bffbe45c64d710160766ced4e7e
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-05-14 10:30:42 +0300

    add path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+      pages: '/src/pages',
+      constants: '/src/constants',
+      images: '/src/images',
+    },
+  },
+  base: '/online-psychological-support',
 })
