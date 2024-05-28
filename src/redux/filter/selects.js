b60107de33909df42e6660b97b9cc4f10866535f
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-05-28 12:36:29 +0300

    created selects

---
+++ src/redux/filter/selects.js
@@ -0,0 +1,5 @@
+export const selectFilteredPsychologistData = (state) =>
+  state.filter.filteredPsychologists;
+export const selectPsychologistFilter = (state) => state.filter.filter;
+export const selectFilteredFavorites = (state) =>
+  state.filter.filteredFavorites;
---
+++ src/redux/psychologists/selects.js
@@ -0,0 +1,2 @@
+export const selectPsychologistData = state => state.psychologists.psychologists;
+export const selectPsychologistIsLoading = state => state.psychologists.isLoading;
