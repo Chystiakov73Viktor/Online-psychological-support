b5e04b3530d98048889ea549e8ab3468f17a8519
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-05-28 12:35:56 +0300

    created reducer

---
+++ src/redux/filter/reducer.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { filterPsychologists } from "./operations";
+
+const filterSlice = createSlice({
+  name: "filter",
+  initialState: {
+    filter: "",
+    filteredPsychologists: [],
+    filteredFavorites: [],
+  },
+  reducers: {
+    setPsychologistsFiltered(state, action) {
+      state.filter = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(filterPsychologists.fulfilled, (state, action) => {
+      const { filteredData, type } = action.payload;
+      if (type === 'favorites') {
+        state.filteredFavorites = filteredData;
+      } else {
+        state.filteredPsychologists = filteredData;
+      }
+    });
+  },
+});
+
+export const { setPsychologistsFiltered } = filterSlice.actions;
+export const filterReducer = filterSlice.reducer;
---
+++ src/redux/psychologists/reducer.js
@@ -0,0 +1,47 @@
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import { getPsychologists, addPsychologists, deletePsychologist } from './operations';
+
+const psychologistsSlice = createSlice({
+  name: 'psychologists',
+  initialState: {
+    psychologists: [],
+    isLoading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: builder =>
+    builder
+      .addCase(getPsychologists.fulfilled, (state, action) => {
+        state.psychologists = action.payload;
+      })
+      .addCase(addPsychologists.fulfilled, (state, action) => {
+        state.psychologists.push(action.payload);
+      })
+      .addCase(deletePsychologist.fulfilled, (state, action) => {
+        state.psychologists = state.psychologists.filter(psychologist =>psychologist.id !== action.payload.id);
+      })
+      .addMatcher(isAnyOf(...getRequests('fulfilled')), handleFulfilled)
+      .addMatcher(isAnyOf(...getRequests('pending')), handlePending)
+      .addMatcher(isAnyOf(...getRequests('rejected')), handleRejected),
+});
+
+const handleFulfilled = state => {
+  state.isLoading = false;
+  state.error = null;
+};
+
+const handlePending = state => {
+  state.isLoading = true;
+  state.error = null;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
+const arrayRequests = [getPsychologists, addPsychologists, deletePsychologist];
+
+const getRequests = type => arrayRequests.map(action => action[type]);
+
+export const psychologistsReducer = psychologistsSlice.reducer;
