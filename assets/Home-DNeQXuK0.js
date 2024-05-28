a57f715e23bea14dccd306ec7c8df51bd592fb5e
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-05-28 09:54:17 +0000

    Deploying to gh-pages from @ Chystiakov73Viktor/online-psychological-support@6006bf0cf4e044acbf1c9c77c8f91a0f853958a8 🚀

---
+++ assets/Home-DNeQXuK0.js
@@ -0,0 +1,177 @@
+import{p as i,G as n,j as e}from"./index-DImMxPOp.js";import{M as o,a,b as r,S as s}from"./index-BN1t9Yqd.js";import{d as c}from"./psychologist-DvuPd2nU.js";const l=i.div`
+  @media screen and (min-width: 768px) {
+    /* display: flex; */
+    /* flex-wrap: wrap; */
+    /* align-items: center; */
+    /* justify-content: center; */
+  }
+
+  @media screen and (min-width: 1440px) {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .containerText {
+    width: 100%;
+    max-width: 595px;
+    margin: auto;
+  }
+
+  @media screen and (min-width: 1440px) {
+    .containerText {
+      margin: 0;
+      margin-right: 20px;
+    }
+  }
+
+  .containerImg {
+    width: 100%;
+    max-width: 464px;
+    position: relative;
+    height: auto;
+    margin: auto;
+  }
+
+  @media screen and (min-width: 1440px) {
+    .containerImg {
+      margin: 0;
+    }
+  }
+
+  .heroTitle {
+    width: 100%;
+    font-weight: 600;
+    font-size: 48px;
+    line-height: 1.025;
+    letter-spacing: -0.02em;
+    color: #191a15;
+    margin-bottom: 20px;
+  }
+
+  .depths {
+    font-style: italic;
+    color: #3470ff;
+  }
+
+  .heroText {
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 1.33333;
+    letter-spacing: -0.02em;
+    color: #191a15;
+    margin-bottom: 40px;
+  }
+
+  .heroButton {
+    border: transparent;
+    border-radius: 30px;
+    padding: 18px 50px;
+    font-family: "Inter", sans-serif;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 1.2;
+    letter-spacing: -0.01em;
+    color: #fbfbfb;
+    background-color: #3470ff;
+    transition: background-color 300ms linear, box-shadow 300ms linear;
+    &:focus,
+    &:hover {
+      background-color: #255ee8;
+    }
+  }
+
+  .containerCheck {
+    /* position: absolute; */
+    bottom: 75px;
+    left: -101px;
+    display: flex;
+    align-items: center;
+    padding: 32px;
+    border-radius: 20px;
+    background-color: #3470ff;
+  }
+
+  .textCheck {
+    font-size: 14px;
+    color: rgba(251, 251, 251, 0.5);
+    margin-bottom: 8px;
+  }
+
+  .textNumber {
+    font-weight: 700;
+    font-size: 24px;
+    color: #fbfbfb;
+  }
+
+  .containerFaCheck {
+    padding: 14px;
+    border-radius: 8px;
+    background-color: #fff;
+    margin-right: 16px;
+  }
+
+  .containerQuestionMark,
+  .containerPeople {
+    position: absolute;
+    border-radius: 10px;
+    width: 40px;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .containerQuestionMark {
+    top: 201px;
+    left: -33px;
+    background: #54be96;
+    transform: rotate(-15deg);
+  }
+
+  .containerPeople {
+    top: 38px;
+    right: -33px;
+    background: #fbc75e;
+    transform: rotate(15deg);
+  }
+
+  @media screen and (max-width: 768px) {
+    .heroTitle {
+      font-size: 36px;
+    }
+
+    .heroText {
+      font-size: 16px;
+    }
+
+    .heroButton {
+      padding: 15px 40px;
+      font-size: 18px;
+    }
+
+    .containerCheck {
+      padding: 15px;
+    }
+
+    .containerQuestionMark,
+    .containerPeople {
+      width: 40px;
+      height: 40px;
+    }
+  }
+
+  @media screen and (min-width: 768px) {
+    .containerCheck {
+      position: absolute;
+    }
+  }
+
+  @media screen and (min-width: 1440px) {
+    .heroTitle {
+      width: 595px;
+      font-size: 80px;
+    }
+  }
+`;function d(t){return n({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"},child:[]}]})(t)}const p=()=>e.jsxs(l,{children:[e.jsxs("div",{className:"containerText",children:[e.jsxs("h1",{className:"heroTitle",children:["The road to the ",e.jsx("span",{className:"depths",children:"depths"})," of the human soul"]}),e.jsx("p",{className:"heroText",children:"We help you to reveal your potential, overcome challenges and find a guide in your own life with the help of our experienced psychologists."}),e.jsxs("button",{className:"heroButton",type:"button",children:["Get started",e.jsx(o,{style:{strokeWidth:"2px",stroke:"transparent",width:"15px",height:"17px",verticalAlign:"middle",marginLeft:"18px",border:"none"}})]})]}),e.jsxs("div",{className:"containerImg",children:[e.jsx("img",{className:"psychologistImg",src:c,alt:"psychologist"}),e.jsxs("div",{className:"containerCheck",children:[e.jsx("div",{className:"containerFaCheck",children:e.jsx(d,{style:{width:"20px",height:"16px",fill:"#3470ff"}})}),e.jsxs("div",{children:[e.jsx("p",{className:"textCheck",children:"Experienced psychologists"}),e.jsx("p",{className:"textNumber",children:"15,000"})]})]}),e.jsx("div",{className:"containerQuestionMark",children:e.jsx(a,{style:{fill:"#fbfbfb"}})}),e.jsx("div",{className:"containerPeople",children:e.jsx(r,{style:{fill:"#fbfbfb"}})})]})]});function f(){return e.jsx(s,{className:"homeSection",titleClassName:"titleHome",children:e.jsx(p,{})})}export{f as default};
---
+++ assets/psychologist-DvuPd2nU.js
@@ -0,0 +1,1 @@
+const s="/online-psychological-support/assets/psychologist-u-rA0agz.jpg";export{s as d};
