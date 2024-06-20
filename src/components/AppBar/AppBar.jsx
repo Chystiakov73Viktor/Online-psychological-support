4d5ec1c498e4a7d9199626e2c7b56ec23702bf8e
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-06-20 23:10:24 +0300

    add @mui/material/AppBar

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,48 +1,160 @@
 import { useState } from 'react';
-import { DivWrapper } from './AppBar.styled';
-import { Container } from '../Container/Container';
-import { Navigation, NavigationMobile } from '../Navigation/Navigation';
-import AuthNav from '../AuthNav/AuthNav';
 import { useSelector } from 'react-redux';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import Tooltip from '@mui/material/Tooltip';
+import { CiLogin } from 'react-icons/ci';
 import { selectUserIsSignIn } from '../../redux/users/selects';
-import UserMenu from '../UserMenu/UserMenu';
 import { Logo } from '../Logo/Logo';
-import { FiMenu } from 'react-icons/fi';
-import { Modal } from 'components/Modal/Modal';
+import { Navigation, NavigationMobile } from '../Navigation/Navigation';
+import UserMenu from '../UserMenu/UserMenu';
+import AuthNav from '../AuthNav/AuthNav';
+import { StyledMenuIcon } from './AppBar.styled';
 
-export const AppBar = () => {
+export const AppBarComponent = () => {
   const isSignin = useSelector(selectUserIsSignIn);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+  const handleOpenNavMenu = event => {
+    setAnchorElNav(event.currentTarget);
   };
 
-  const closeMenu = () => {
-    setIsMenuOpen(false);
+  const handleOpenUserMenu = event => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
   };
 
   return (
-    <Container>
-      <DivWrapper>
-        <Logo />
-        <div className="mobileMenuIcon" onClick={toggleMenu}>
-          <FiMenu size="24" />
-        </div>
-        {isMenuOpen && (
-          <Modal
-            onClose={closeMenu}
-            backdropClassName="backdropNavigationMobile"
-            contentClassName="contentNavigationMobile"
+    <AppBar position="static" sx={{ backgroundColor: '#fff' }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
           >
-            <NavigationMobile setIsMenuOpen={setIsMenuOpen} />
-          </Modal>
-        )}
-        <Navigation />
-        <div className="logWrapper">
-          {isSignin ? <UserMenu /> : <AuthNav />}
-        </div>
-      </DivWrapper>
-    </Container>
+            <Logo />
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <StyledMenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              <NavigationMobile setIsMenuOpen={setAnchorElNav} />
+            </Menu>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            <Logo />
+          </Typography>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: 'none', md: 'flex' },
+              justifyContent: 'center',
+            }}
+          >
+            <Navigation />
+          </Box>
+
+          <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
+            {isSignin ? <UserMenu /> : <AuthNav />}
+          </Box>
+
+          <Box sx={{ flexGrow: 0, display: { xs: 'flex', md: 'none' } }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <CiLogin size="32" />
+              </IconButton>
+            </Tooltip>
+            {anchorElUser !== null && (
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {isSignin ? <UserMenu /> : <AuthNav />}
+              </Menu>
+            )}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
+
+export default AppBarComponent;
