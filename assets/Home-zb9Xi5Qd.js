693b7edf8271000cf53d8463b0449c5cbaed4279
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-06-20 20:11:21 +0000

    Deploying to gh-pages from @ Chystiakov73Viktor/online-psychological-support@4d5ec1c498e4a7d9199626e2c7b56ec23702bf8e 🚀

--- assets/Home-zb9Xi5Qd.js
+++ assets/Home-zb9Xi5Qd.js
@@ -1,4 +1,4 @@
-import{p as i,G as n,j as e,L as o}from"./index-CeQr3ZH5.js";import{M as a,a as r,b as s,S as c}from"./index-BLAmpNHH.js";import{d as l}from"./psychologist-DvuPd2nU.js";const d=i.div`
+import{p as i,G as n,j as e,L as o}from"./index-Uy0fBLzU.js";import{M as a,a as r,b as s,S as c}from"./index-Pqg2k8k2.js";import{d as l}from"./psychologist-DvuPd2nU.js";const d=i.div`
   @media screen and (min-width: 768px) {
     /* display: flex; */
     /* flex-wrap: wrap; */
@@ -17,12 +17,14 @@ import{p as i,G as n,j as e,L as o}from"./index-CeQr3ZH5.js";import{M as a,a as 
     width: 100%;
     max-width: 595px;
     margin: auto;
+    margin-bottom: 20px;
   }
 
   @media screen and (min-width: 1440px) {
     .containerText {
       margin: 0;
       margin-right: 20px;
+      margin-bottom: 0;
     }
   }
 
@@ -32,6 +34,16 @@ import{p as i,G as n,j as e,L as o}from"./index-CeQr3ZH5.js";import{M as a,a as 
     position: relative;
     height: auto;
     margin: auto;
+  }
+
+  .psychologistImg {
+    margin-bottom: 20px;
+  }
+
+  @media screen and (min-width: 768px) {
+    .psychologistImg {
+      margin-bottom: 0;
+    }
   }
 
   @media screen and (min-width: 1440px) {
@@ -65,10 +77,12 @@ import{p as i,G as n,j as e,L as o}from"./index-CeQr3ZH5.js";import{M as a,a as 
   }
 
   .heroButton {
+    display: block;
+    width: 240px;
     border: transparent;
     border-radius: 30px;
     padding: 18px 50px;
-    font-family: "Inter", sans-serif;
+    font-family: 'Inter', sans-serif;
     font-weight: 500;
     font-size: 20px;
     line-height: 1.2;
@@ -174,4 +188,4 @@ import{p as i,G as n,j as e,L as o}from"./index-CeQr3ZH5.js";import{M as a,a as 
       font-size: 80px;
     }
   }
-`;function p(t){return n({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"},child:[]}]})(t)}const h=()=>e.jsxs(d,{children:[e.jsxs("div",{className:"containerText",children:[e.jsxs("h1",{className:"heroTitle",children:["The road to the ",e.jsx("span",{className:"depths",children:"depths"})," of the human soul"]}),e.jsx("p",{className:"heroText",children:"We help you to reveal your potential, overcome challenges and find a guide in your own life with the help of our experienced psychologists."}),e.jsxs(o,{to:"/psychologists",className:"heroButton",style:{textDecoration:"none"},children:["Get started",e.jsx(a,{style:{strokeWidth:"2px",stroke:"transparent",width:"15px",height:"17px",verticalAlign:"middle",marginLeft:"18px",border:"none"}})]})]}),e.jsxs("div",{className:"containerImg",children:[e.jsx("img",{className:"psychologistImg",src:l,alt:"psychologist"}),e.jsxs("div",{className:"containerCheck",children:[e.jsx("div",{className:"containerFaCheck",children:e.jsx(p,{style:{width:"20px",height:"16px",fill:"#3470ff"}})}),e.jsxs("div",{children:[e.jsx("p",{className:"textCheck",children:"Experienced psychologists"}),e.jsx("p",{className:"textNumber",children:"15,000"})]})]}),e.jsx("div",{className:"containerQuestionMark",children:e.jsx(r,{style:{fill:"#fbfbfb"}})}),e.jsx("div",{className:"containerPeople",children:e.jsx(s,{style:{fill:"#fbfbfb"}})})]})]});function g(){return e.jsx(c,{className:"homeSection",titleClassName:"titleHome",children:e.jsx(h,{})})}export{g as default};
+`;function p(t){return n({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"},child:[]}]})(t)}const h=()=>e.jsxs(d,{children:[e.jsxs("div",{className:"containerText",children:[e.jsxs("h1",{className:"heroTitle",children:["The road to the ",e.jsx("span",{className:"depths",children:"depths"})," of the human soul"]}),e.jsx("p",{className:"heroText",children:"We help you to reveal your potential, overcome challenges and find a guide in your own life with the help of our experienced psychologists."}),e.jsxs(o,{to:"/psychologists",className:"heroButton",style:{textDecoration:"none"},children:["Get started",e.jsx(a,{style:{strokeWidth:"2px",stroke:"transparent",width:"15px",height:"17px",verticalAlign:"middle",marginLeft:"18px",border:"none"}})]})]}),e.jsxs("div",{className:"containerImg",children:[e.jsx("img",{className:"psychologistImg",src:l,alt:"psychologist"}),e.jsxs("div",{className:"containerCheck",children:[e.jsx("div",{className:"containerFaCheck",children:e.jsx(p,{style:{width:"20px",height:"16px",fill:"#3470ff"}})}),e.jsxs("div",{children:[e.jsx("p",{className:"textCheck",children:"Experienced psychologists"}),e.jsx("p",{className:"textNumber",children:"15,000"})]})]}),e.jsx("div",{className:"containerQuestionMark",children:e.jsx(r,{style:{fill:"#fbfbfb"}})}),e.jsx("div",{className:"containerPeople",children:e.jsx(s,{style:{fill:"#fbfbfb"}})})]})]});function f(){return e.jsx(c,{className:"homeSection",titleClassName:"titleHome",children:e.jsx(h,{})})}export{f as default};
